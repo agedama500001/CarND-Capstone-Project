a53063906de56fbc04e397b74a00d891328f3a8a
agedama500001 <agedama500001@gmail.com>
2019-07-24 05:48:08 -0700

    rqt-graph result add.

--- ros/build/catkin_generated/setup_cached.sh
+++ ros/build/catkin_generated/setup_cached.sh
@@ -7,7 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/student/Desktop/CarND-Capstone-Project/ros/devel:$CMAKE_PREFIX_PATH"
 export PWD="/home/student/Desktop/CarND-Capstone-Project/ros/build"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/student/Desktop/CarND-Capstone-Project/ros/devel/share/common-lisp"
-export ROS_PACKAGE_PATH="/home/student/Desktop/CarND-Capstone-Project/ros/src:$ROS_PACKAGE_PATH"
